bf6b14668bc9bdbd72a54d8037f97ff1575c3f2e
jobil <jthomas@pole3.com>
2024-07-20 19:12:30 +0530

    integrated can filter and can json

--- canson/canFilter.py
+++ canson/canFilter.py
@@ -1,6 +1,7 @@
 import time
 import can
 import threading
+from .canson import CANson
 
 __all__ = ["CANFilter"]
 
@@ -21,20 +22,41 @@ class CANFilter:
         self.filters = filters
         self.filters_changed = False
         self.timeout = 1
+        self.__gui_list = []
+        self.max_num_entry = 10
+        self._key_lock = threading.Lock()
 
     def set_filters(self, filters):
         self.bus.set_filters(filters)
 
-    def recv(self):
+    def get_gui_list(self):
+        with self._key_lock:
+            return self.__gui_list
+
+    def set_gui_list(self, gui_list):
+        with self._key_lock:
+            self.__gui_list = gui_list
+
+    def __append_gui_list(self, name, data):
+        gui_list = self.get_gui_list()
+        if len(gui_list) < self.max_num_entry:
+            gui_list.append([name, data])
+        else:
+            gui_list.pop(0)
+            gui_list.append([name, data])
+        self.set_gui_list(gui_list)
+
+    def recv(self, canson: CANson):
         while True:
-            if self.filters_changed:
-                self.bus.set_filters(self.filters)
-                print("filters changed")
-                self.filters_changed = False
             msg = self.bus.recv(timeout=self.timeout)
             if msg != None:
-                print(msg.arbitration_id)
-                print(msg)
+                # print(msg.arbitration_id)
+                # print(msg.data)
+                # print(msg)
+                frame = canson.get_frame(msg.arbitration_id)
+                name = canson.get_frame_name(frame)
+                data = canson.get_frame_data(frame, msg.data)
+                self.__append_gui_list(name, data)
 
 
 def filter():
--- canson/canson.py
+++ canson/canson.py
@@ -1,61 +1,117 @@
 import json
+import sys
+import os
+
 
 __all__ = ["CANson"]
+canson_dir = os.path.dirname(__file__)
+# Construct the full path to the JSON file
+json_path = os.path.join(canson_dir, "canson.json")
 
 
 class CANson:
-    def __init__(self, config_file="canson.json") -> None:
+    def __init__(self, config_file=json_path) -> None:
         self.config_file = config_file
         self.frame_desc = json.load(open(self.config_file))
+        self.mask_dict = {True: 0x1FFFFFFF, False: 0x7FF}
+        self.valid_frame_id_dict = {True: 2**29, False: 2**11}
+        self.valid_frame_list = self.__get_valid_frames()
 
-    def get_frame_index(self, frame_id):
-        i = 0
-        for frame in self.frame_desc:
-            if frame["id"] == frame_id:
-                break
-            i += 1
-        return i
+    def get_frame(self, frame_id):
+        for frame in self.valid_frame_list:
+            if int(frame["id"], 0) == frame_id:
+                return frame
+        return None
 
-    def get_frame_name(self, index):
-        frame_name = ""
-        try:
-            frame_name = self.frame_desc[index]["name"]
-        except IndexError:
-            frame_name = "unkown"
-        return frame_name
+    def get_frame_name(self, frame):
+        return frame["name"]
 
-    def get_frame_type(self, index):
-        frame_type = ""
-        try:
-            frame_type = self.frame_desc[index]["type"]
-        except IndexError:
-            frame_type = "unknown"
-        return frame_type
+    def get_frame_type(self, frame):
+        return frame["type"]
 
-    def get_frame_data(self, frame_type, index, data):
-        frame_data = None
-        if frame_type == "int":
-            frame_data = str(int.from_bytes(data, "little"))
-        elif frame_type == "enum":
-            key = str(int.from_bytes(data, "little"))
-            if "categories" in self.frame_desc[index]:
-                dict = self.frame_desc[index]["categories"]
-                if key in dict:
-                    frame_data = self.frame_desc[index]["categories"][key]
-                else:
-                    print("enum out of range")
+    def __isValid(self, frame):
+        if not all(key in frame for key in ["name", "id", "type"]):
+            return False
+        if not isinstance(frame["name"], (str)):
+            return False
+        extended = False
+        if "extended" in frame:
+            ext = frame["extended"]
+            if not isinstance(ext, (bool)):
+                return False
             else:
-                print("enum not defined in json file")
-        elif frame_type == "intList":
-            frame_data = str(list(data))
+                extended = ext
+        try:
+            frame_id = int(frame["id"], 0)
+            if not (0 <= frame_id < self.valid_frame_id_dict[extended]):
+                return False
+        except ValueError:
+            return False
+
+        if frame["type"] not in ["int", "intlist", "enum"]:
+            return False
+        elif frame["type"] == "enum":
+            if not "categories" in frame:
+                return False
+            elif not isinstance(frame["categories"], (dict)):
+                return False
+        return True
+
+    def __get_valid_frames(self):
+        valid_frames = []
+        for frame in self.frame_desc:
+            if self.__isValid(frame):
+                valid_frames.append(frame)
+        return valid_frames
+
+    def get_frame_data(self, frame, data):
+        frame_type = self.get_frame_type(frame)
+
+        match frame_type:
+
+            case "int":
+                frame_data = str(int.from_bytes(data, "little"))
+
+            case "enum":
+                key = str(int.from_bytes(data, "little"))
+                enum_dict = frame["categories"]
+                if key in enum_dict:
+                    frame_data = frame["categories"][key]
+                else:
+                    frame_data = None
+                    print("enum out of range")
+
+            case "intlist":
+                frame_data = str(list(data))
+
+            case _:
+                frame_data = None
+
         return frame_data
+
+    def get_filters(self):
+        filter_list = []
+        for frame in self.valid_frame_list:
+            extended = False
+            if "extended" in frame:
+                extended = frame["extended"]
+            frame_id = int(frame["id"], 0)
+            filter_list.append(
+                {
+                    "can_id": frame_id,
+                    "can_mask": self.mask_dict[extended],
+                    "extended": extended,
+                }
+            )
+        return filter_list
 
 
 if __name__ == "__main__":
     cs = CANson()
     data = 2
-    index = cs.get_frame_index(101)
-    print(cs.get_frame_name(index))
-    print(
-        cs.get_frame_data(cs.get_frame_type(index), index, data.to_bytes(4, "little"))
-    )
+    frame = cs.get_frame(0x101)
+    print(cs.get_frame_name(frame))
+    print(cs.get_frame_data(frame, data.to_bytes(4, "little")))
+    print(cs.get_frame_type(frame))
+    print(cs.get_filters())
+    print(cs.valid_frame_list)
